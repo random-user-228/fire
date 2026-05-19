27b28227aa83c615eeb9ecd4a4c21907491b041f
oleksiykholod <oleksiykholod@gmail.com>
2026-05-19 22:12:43 +0300

    pwa

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,13 @@ const withPWA = withPWAInit({
   cacheOnFrontEndNav: true,        // Кешувати сторінки при переході по Link
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
+  workboxOptions: {
+    // 1. Явно додаємо 'pdf' у список розширень для сканування в папці public
+    additionalManifestEntries: [{ url: '/116.pdf', revision: null }],
+    
+    // 2. Збільшуємо ліміт до 50 Мегабайт, щоб великі PDF-інструкції не ігнорувалися при кешуванні
+    maximumFileSizeToCacheInBytes: 50 * 1024 * 1024, 
+  }
 });
 
 const nextConfig: NextConfig = {
